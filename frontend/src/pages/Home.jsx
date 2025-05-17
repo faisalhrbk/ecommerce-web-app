092f9c1251f4cd45848d8d34e9fb5950eb9d60de
faisalhrbk <faisalhrbk@outlook.com>
2025-05-17 12:30:27 +0500

    gender collection section done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 
 import Hero from "../components/layout/Hero"
+import GenderCollection from "../components/products/GenderCollection"
 const Home = () => {
   return (
     <div>
         <Hero/>
+        
+        {/* gender collection for home page */}
+        <GenderCollection/>
     </div>
   )
 }
