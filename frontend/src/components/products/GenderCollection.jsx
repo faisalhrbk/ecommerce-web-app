e94f44fbfd268663b0de0f71200a2b9088689a41
faisalhrbk <faisalhrbk@outlook.com>
2025-05-17 22:10:06 +0500

    half done best seller section

--- frontend/src/components/products/GenderCollection.jsx
+++ frontend/src/components/products/GenderCollection.jsx
@@ -1,37 +1,49 @@
 import { Link } from "react-router-dom";
 import mensCollectionImg from "../../assets/mens-collection.webp";
 
-
 const GenderCollection = () => {
     return (
-        <section className="py-16 px-4 lg:-x-0">
-            <div className="container mx-auto flex flex-col md:flex-row gap-8">
+        <section className="py-8 md:py-16 px-4 max-w-7xl mx-auto">
+            <h1 className="text-center mb-8 text-2xl md:text-4xl font-bold">
+                Our Collections
+            </h1>
+            <div className="flex flex-col md:flex-row gap-6">
                 {/* women collection */}
-                <div className="relative flex-1">
+                <div className="relative flex-1 mb-4 md:mb-0">
                     <img
                         src={mensCollectionImg}
                         alt="women collection img"
-                        className="w-full h-[700px] object-cover"
+                        className="w-full h-[250px] md:h-[400px] rounded object-cover px-4"
                     />
-                    <div className="absolute bottom-8 left-8 bg-white/90  p-4">
-                        <h2 className="text-2xl font-bold text-gray-900 mb-3">
+                    <div className="absolute bottom-4 left-8 bg-white/90 p-3 rounded shadow-sm">
+                        <h2 className="text-xl md:text-2xl font-bold text-gray-900 mb-2">
                             Women's Collection
                         </h2>
-                        <Link to="/collection/all?gender=Women" className="text-gray-800 cursor-pointer hover:text-gray-900 hover:font-bold transition-all duration-500 underline">Shop Now</Link>
+                        <Link
+                            to="/collection/all?gender=Women"
+                            className="text-gray-800 hover:font-bold underline"
+                        >
+                            Shop Now
+                        </Link>
                     </div>
                 </div>
                 {/* mens collection */}
                 <div className="relative flex-1">
                     <img
                         src={mensCollectionImg}
-                        alt="women collection img"
-                        className="w-full h-[700px] object-cover"
+                        alt="men collection img"
+                        className="w-full h-[250px] md:h-[400px] rounded object-cover px-4"
                     />
-                    <div className="absolute bottom-8 left-8 bg-white/90  p-4">
-                        <h2 className="text-2xl font-bold text-gray-900 mb-3">
-                            Mens's Collection
+                    <div className="absolute bottom-4 left-8 bg-white/90 p-3 rounded shadow-sm">
+                        <h2 className="text-xl md:text-2xl font-bold text-gray-900 mb-2">
+                            Men's Collection
                         </h2>
-                        <Link to="/collection/all?gender=Men" className="text-gray-800 cursor-pointer hover:text-gray-900 hover:font-bold transition-all duration-500 underline">Shop Now</Link>
+                        <Link
+                            to="/collection/all?gender=Men"
+                            className="text-gray-800 hover:font-bold underline"
+                        >
+                            Shop Now
+                        </Link>
                     </div>
                 </div>
             </div>
--- frontend/src/components/products/ProductDetails.jsx
+++ frontend/src/components/products/ProductDetails.jsx
@@ -26,7 +26,7 @@ const ProductDetails = () => {
 			<div className="max-w-6xl mx-auto bg-white p-8 rounded-lg">
 				<div className="flex flex-col md:flex-row">
 					{/* Left Thumb */}
-					<div className="flex flex-col space-y-4 mr-6">
+					<div className="md:flex flex-col space-y-4 mr-6 hidden ">
 						{selectedProduct.images.map((image, index) => (
 							<img
 								key={index}
@@ -38,7 +38,7 @@ const ProductDetails = () => {
 					</div>
 
 					{/* main image */}
-					<div className="md:w-1/2">
+					<div className="md:w-1/2 w-3/5">
 						<div className="mb-4">
 							<img
 								src={selectedProduct.images[0]?.url}
@@ -56,6 +56,73 @@ const ProductDetails = () => {
 									className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
 								/>
 							))}
+						</div>
+					</div>
+
+					{/* right section */}
+					<div className="md:w-1/2 md:ml-10">
+						<h1 className="text-2xl md:text-3xl font-semibold mb-2">
+							{selectedProduct.name}
+						</h1>
+						<p className="text-lg text-gray-600 mb-1 line-through">
+							{selectedProduct.originalPrice &&
+								`$${selectedProduct.originalPrice}`}
+						</p>
+						<p className="text-xl text-gray-500 mb-2">
+							${selectedProduct.price}
+						</p>
+						<p className="text-gray-600 mb-4">{selectedProduct.description}</p>
+						<div className="mb-4">
+							<p className="text-gray-700">Color:</p>
+							<div className="flex gap-2 mt-2">
+								{selectedProduct.color.map((color) => (
+									<button
+										className="w-8 h-8 rounded-full border"
+										key={color}
+										style={{ backgroundColor: color.toLowerCase() }}
+									></button>
+								))}
+							</div>
+						</div>
+						<div className="mb-4">
+							<p className="text-gray-700">Size:</p>
+							<div className="flex gap mt-2">
+								{selectedProduct.sizes.map((size) => {
+									<button key={size} className="px-4 py-2 rounded border">
+										{size}
+									</button>;
+								})}
+							</div>
+						</div>
+						<div className="mb-6">
+							<p className="text-gray-700 ">Quantity</p>
+							<div className="flex items-center space-x-4 mt-2">
+								<button className="px-2 py-1 bg-gray-200 rounded text-lg">
+									-
+								</button>
+								<span className="text-lg">2</span>
+								<button className="px-2 py-1 bg-gray-200 rounded text-lg">
+									+
+								</button>
+							</div>
+						</div>
+						<button className="bg-black text-white py-2 px-6 rounded w-full mb-4 ">
+							Add to Cart
+						</button>
+						<div className="mt-10 text-gray-700">
+							<h3 className="text-xl font-bold mb-4"> Characteristics:</h3>
+							<table className="w-full text-left text-sm text-gray-600">
+								<tbody>
+									<tr>
+										<td className="py-1">Brand</td>
+										<td className="py-1">{selectedProduct.brand}</td>
+									</tr>
+									<tr>
+										<td className="py-1">Material</td>
+										<td className="py-1">{selectedProduct.material}</td>
+									</tr>
+								</tbody>
+							</table>
 						</div>
 					</div>
 				</div>
