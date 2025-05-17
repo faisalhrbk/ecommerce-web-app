ffcc6d37c01322dc7f84451ca3a02950f8984310
faisalhrbk <faisalhrbk@outlook.com>
2025-05-17 12:06:53 +0500

    compleated hero sechtion

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
-
 import UserLayout from "./components/layout/UserLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
 	return (
-	
 		<BrowserRouter>
 			<Routes>
+				{/*User Layout*/}
 				<Route path="/" element={<UserLayout />}>
-					{/*User Layout*/}
+					<Route index element={<Home />} />
 				</Route>
+
 				<Route>{/*Admin Layout*/}</Route>
 			</Routes>
 		</BrowserRouter>
-
 	);
 };
 
 export default App;
-
-
--- frontend/src/components/layout/UserLayout.jsx
+++ frontend/src/components/layout/UserLayout.jsx
@@ -1,18 +1,20 @@
+import { Outlet } from "react-router-dom";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 
 const UserLayout = () => {
 	return (
 		<>
-	
 			{/* Header */}
 			<Header />
 
 			{/* Main Content */}
+			<main>
+				<Outlet />
+			</main>
 
 			{/* Footer */}
-			<Footer/>
-
+			<Footer />
 		</>
 	);
 };
